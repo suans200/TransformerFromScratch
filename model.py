d841222cd47dcea4ddf02177b0eed684c8db6b3d
subhankar_2003 <subhankar.official185@gmail.com>
2025-06-07 15:00:40 +0530

    Multihead attention  Block class added

--- model.py
+++ model.py
@@ -50,6 +50,50 @@ class FeedForwardBlock(nn.Module):
     def forward(self, x):
         return self.linear2(self.dropout(self.linear1(x)))
 
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, h:int, dropout:float):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = dropout
+        assert self.d_model%self.h == 0, "d_model is not divisible by h."
+        self.d_k = self.d_model//self.h
+        self.w_q = nn.Linear(self.d_model, self.d_model)
+        self.w_k = nn.Linear(self.d_model, self.d_model)
+        self.w_v = nn.Linear(self.d_model, self.d_model)
+        self.w_o = nn.Linear(self.d_model, self.d_model)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout:nn.Dropout):
+        d_k = query.shape[-1]
+        attention_score = (query@key.transpose(-2,-1))/ math.sqrt(d_k)
+        if mask is not None:
+            attention_score.masked_fill_(mask == 0, -1e9)
+        attention_score = attention_score.softmax(-1)
+        if dropout is not None:
+            attention_score = dropout(attention_score)
+        return (attention_score@value), attention_score
+    
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        query = query.view(query.shape[0],query.shape[1], self.h, self.d_k).transpose(1,2)
+        key = key.view(key.shape[0],key.shape[1], self.h, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0],value.shape[1], self.h, self.d_k).transpose(1,2)
+
+        x, self.attenstion_score = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+        x = x.transpose(1,2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        return self.w_o(x)
+
+
+
+
+
+
+
 
 
 
